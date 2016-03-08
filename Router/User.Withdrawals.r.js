1f9827744d0f2628efd83d3c744bd75b5a2b6723
Gaubee <gaubeebangeel@gmail.com>
2016-03-08 19:02:35 +0800

    v2.5.5 增加在线支付回调的接口
    Signed-off-by: Gaubee <gaubeebangeel@gmail.com>

--- Router/User.Withdrawals.r.js
+++ Router/User.Withdrawals.r.js
@@ -1,11 +1,37 @@
 exports.install = install;
 var co = require("co");
 var Context = require("../socket_handles/context");
+var RedisClient = require("../Model/redis_index");
 
 function install(socket, waterline_instance, classMap) {
 	"use strict";
 	var routers = {
 		"prefix": "/user/withdrawals",
+		"get": {
+			"/online_pay_callback/:key": [{
+				doc: {
+					des: "用户在线充值后的回调URL"
+				},
+				emit_with: ["params"]
+			}, function*(data) {
+				var key = data.params.key;
+				if (!key) {
+					Throw("ref", "params.key is null")
+				}
+				var redis_client = yield RedisClient.getClient();
+				var pay_json_data = yield redis_client.thunk.GET([key]);
+				const prefix = "ONLINE_PAY_CB@";
+				if (!pay_json_data || pay_json_data.indexOf(prefix) === 0) {
+					Throw("ref", "params.key hasn't Reference Data");
+				}
+				try {
+					pay_json_data = JSON.parse(pay_json_data.substr(prefix.length));
+				} catch (e) {
+					Throw("ref", "JSON_Data NO Work");
+				}
+				this.body = pay_json_data;
+			}]
+		},
 		"post": {
 			"/apply": [{
 				doc: {
@@ -31,13 +57,15 @@ function install(socket, waterline_instance, classMap) {
 			"/confirm_arrive": [{
 				doc: {
 					des: "用户确认到帐",
-					params: [/*{
-						name: "[form.permis_password]",
-						type: "string",
-						des: "二级密码"
-					}*/],
+					params: [
+						/*{
+												name: "[form.permis_password]",
+												type: "string",
+												des: "二级密码"
+											}*/
+					],
 				},
-				emit_with: ["session"/*, "form"*/]
+				emit_with: ["session" /*, "form"*/ ]
 			}, function*(data) {
 				var user_loginer = yield this.user_loginer;
 				// user_loginer._checkPermisPassword(data.form.permis_password);
