129e3da23fc276d46d1022e74ef413cb256e4301
Gaubee <gaubeebangeel@gmail.com>
2016-03-15 14:40:41 +0800

    实现支付回调
    Signed-off-by: Gaubee <gaubeebangeel@gmail.com>

--- Router/User.Withdrawals.r.js
+++ Router/User.Withdrawals.r.js
@@ -19,6 +19,7 @@ var curl = function(url) {
 		}).on('error', reject);
 	});
 };
+const wx_pay_prefix = "ONLINE_PAY_CB@";
 
 function install(socket, waterline_instance, classMap) {
 	"use strict";
@@ -36,19 +37,20 @@ function install(socket, waterline_instance, classMap) {
 					Throw("ref", "params.key is null")
 				}
 				var redis_client = yield RedisClient.getClient();
-				var pay_json_data = yield redis_client.thunk.GET([key]);
-				const prefix = "ONLINE_PAY_CB@";
-				if (!pay_json_data || pay_json_data.indexOf(prefix) === 0) {
+				var pay_key = wx_pay_prefix + key;
+				var pay_json_data = yield redis_client.thunk.GET([pay_key]);
+				if (!pay_json_data) {
 					Throw("ref", "params.key hasn't Reference Data");
 				}
 				try {
-					pay_json_data = JSON.parse(pay_json_data.substr(prefix.length));
+					pay_json_data = JSON.parse(pay_json_data.substr(wx_pay_prefix.length));
 				} catch (e) {
 					Throw("ref", "JSON_Data NO Work");
 				}
-				yield redis_client.thunk.DEL([key]);
+				yield redis_client.thunk.DEL([pay_key]);
+				var amount = pay_json_data.config.total_fee / 100 * /*微信手续费0.06%*/ 0.0006;
 
-				this.body = pay_json_data;
+				this.body = yield user._recharge(amount, `用户使用微信充值，额度：${amount}`);
 			}]
 		},
 		"post": {
@@ -70,20 +72,29 @@ function install(socket, waterline_instance, classMap) {
 				var user_loginer = yield this.user_loginer;
 				var url = "http://" + (is_dev ? "121.40.18.23" : "127.0.0.1") + ":4100/unifiedorderRequest?";
 				var id_prefix = Math.random().toString(36).substr(2, 6);
-				var qs = querystring.stringify({
-					body: "用户充值",
+				var qs_obj = {
+					body: "用户微信充值",
 					out_trade_no: id_prefix + user_loginer.id,
-					total_fee: data.form.amount * 100,
+					total_fee: parseInt((data.form.amount * 100), 10),
 					spbill_create_ip: "127.0.0.1",
 					trade_type: "NATIVE",
 					device_info: "WEB",
 					product_id: id_prefix + data.form.amount
-				});
+				};
+				var qs = querystring.stringify(qs_obj);
 				// console.log(url + qs);
 				var res = yield curl(url + qs);
 				res = JSON.parse(res);
 				// console.log(res)
 				if (res.codeUrl) {
+					var redis_client = yield RedisClient.getClient();
+					redis_client.thunk.SETEX([wx_pay_prefix + qs_obj.out_trade_no,
+						60 * 30 /*半小时支付时间*/ ,
+						JSON.stringify({
+							config: qs_obj,
+							pay_info: res
+						})
+					]);
 					this.body = res.codeUrl;
 				} else {
 					Throw("syntax", "WeiXin 支付服务器出错")
